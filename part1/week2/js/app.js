9be129d3a66a9170403fbc7e22e8d3b76e4302bd
Greg <gohan@Gregs-MBP-2.attlocal.net>
2022-11-14 13:03:30 -0500

    html,js, css examples

---
+++ part1/week2/js/app.js
@@ -0,0 +1,42 @@
+const navBarHeader = document.getElementById('navbarHeader')
+
+console.log(navBarHeader)
+navBarHeader.innerHTML = 'Greg'
+
+helloWorld()
+
+function helloWorld() {
+    const card = document.getElementById('friends')
+    const name = "greg's brewery"
+    const number = 7
+    const boolean = true
+    const arrFriends = ['Greg', 'Andre', 'Luis']
+    const obj = {
+        name: 'Greg',
+        color: 'Blue',
+        hometown: "Dallas, Tx"
+    }
+
+    // for(let i = 0; i < arrFriends.length; i++) {
+    //     card.innerHTML = arrFriends[i]
+
+    // }
+
+    console.log('Hello World')
+}
+
+
+const nameOfTeacher = 'Greg'
+
+function sayHi(item) {
+    debugger;
+    alert('Hello '  + nameOfTeacher )
+    console.log(`Hello ${item}`)
+}
+
+
+sayHi(nameOfTeacher, 'Greg', false)
+
+
+
+
