1cd4e71311b9a0c84d706dd29f28479c98ec7424
Greg <gohan@Gregs-MBP-2.attlocal.net>
2023-03-06 16:36:55 -0500

    todo app

--- part2/giphy_app/src/App.js
+++ part2/giphy_app/src/App.js
@@ -2,28 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './components/partials/Footer.js'
 import { Button } from 'reactstrap'
+import ToDoClass from './components/Todo/ToDoClass';
+import ToDoHook from './components/Todo/ToDoHook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React is fun
-        </a>
+
+        <ToDoClass />
         <Button color='primary'>Hello World</Button>
+        <ToDoHook />
       </header>
-
-
-
       <Footer />
     </div>
   );
